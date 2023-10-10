05faf1b2882ea28c6f3bc803b2e246321390f241
Lalit Narnaulia <k_lalit@outlook.com>
2023-10-10 19:40:10 +0100

    Fix - Search by image fails with error FileNotFoundError
    Migrated to new Weaviate client
    
    feedback changes

--- data.py
+++ data.py
@@ -1,106 +1,92 @@
-'''This file contains code that adds data to weaviate from the Images folder.
+"""This file contains code that adds data to weaviate from the Images folder.
 These images will be the ones with which the module multi2-vec-clip will compare
-the image or text query given by the user.'''
+the image or text query given by the user."""
+import base64
 
-import pickle
 import weaviate
-import uuid
-import datetime
-import base64, json, os
+import os
+from pathlib import Path
 
-def generate_uuid(class_name: str, identifier: str,
-                  test: str = 'teststrong') -> str:
-    """ Generate a uuid based on an identifier
-    :param identifier: characters used to generate the uuid
-    :type identifier: str, required
-    :param class_name: classname of the object to create a uuid for
-    :type class_name: str, required
-    """
-    test = 'overwritten'
-    return str(uuid.uuid5(uuid.NAMESPACE_DNS, class_name + identifier))
+from weaviate import Config
+import weaviate.classes as wvc
+from weaviate.collection.classes.config import VectorIndexType, Multi2VecField
 
-client = weaviate.Client("http://localhost:8080")
-print("Client created")
+# initiating the Weaviate client
+WEAVIATE_URL = os.getenv('WEAVIATE_URL')
+if not WEAVIATE_URL:
+    WEAVIATE_URL = 'http://localhost:8080'
+print(WEAVIATE_URL, flush=True)
 
-#Checking if caption schema already exists, then delete it
-current_schemas = client.schema.get()['classes']
-for schema in current_schemas:
-    if schema['class']=='ClipExample':
-        client.schema.delete_class('ClipExample')
-# Create a schema to add images
-# I have used the web page https://weaviate.io/developers/weaviate/v1.11.0/retriever-vectorizer-modules/multi2vec-clip.html
+client = weaviate.Client(WEAVIATE_URL,
+                         additional_config=Config(grpc_port_experimental=50051))
+print(f"Client is ready {client.is_ready()}")
+
+# delete the ClipExample collection if already exists
+client.collection.delete(["ClipExample"])
+
+# creating the ClipExample collection to add images
+# I have used the https://weaviate.io/developers/weaviate/modules/retriever-vectorizer-modules/multi2vec-clip
 # to get help on making a suitable schema. You can read the contents of this web page to know more.
-class_obj = {
-    "class": "ClipExample",
-        "description": "A class to implement CLIP example",
-        "moduleConfig": {
-        "multi2vec-clip": {
-          "imageFields": [
-              "image"
-          ],
-          "textFields": [
-              "text"
-          ],
-          "weights": {
-            "textFields": [0.7],
-            "imageFields": [0.3]
-          }
-        }
-      },
-        "vectorIndexType": "hnsw",
-        "vectorizer": "multi2vec-clip",
-        "properties": [
-            {
-            "dataType": [
-                "string"
-            ],
-            "name": "text"
-            },
-            {
-            "dataType": [
-                "blob"
-            ],
-            "name": "image"
-            }
-        ]
-    }
-
-client.schema.create_class(class_obj)
-print("Schema class created")
-
+images = client.collection.create(
+    name="ClipExample",
+    description="Implement CLIP example",
+    properties=[
+        wvc.Property(
+            name="text",
+            data_type=wvc.DataType.TEXT,
+        ),
+        wvc.Property(
+            name="image",
+            data_type=wvc.DataType.BLOB,
+        ),
+    ],
+    # the img2vec-neural Weaviate module
+    vectorizer_config=wvc.ConfigFactory.Vectorizer.multi2vec_clip(
+        image_fields=[Multi2VecField(name="image", weight=0.3)],
+        text_fields=[Multi2VecField(name="text", weight=0.7)]),
+    vector_index_type=VectorIndexType.HNSW
+)
+print("The 'ClipExample' class has been defined.")
 
 # Adding all images from static/Images folder
-for img in os.listdir("static/Images/"):
-    
-    encoded_image = weaviate.util.image_encoder_b64(f"static/Images/{img}")
-    
-    data_properties = {
-        "image": encoded_image,
-        "text":img
-    }
-    client.data_object.create(data_properties, "ClipExample", generate_uuid('ClipExample',img))
+images_to_add = []
+imgdir = Path("static/Images/")
+for filepath in imgdir.glob("*.jfif"):
+    img_file_encoded = base64.b64encode(filepath.read_bytes()).decode()
+    image = wvc.DataObject(
+        properties={
+            "image": img_file_encoded,
+            "text": filepath.name
+        })
+    images_to_add.append(image)
+
+images.data.insert_many(images_to_add)
 print("Images added")
 
 # You can try uncommenting the below code to add text as well
 # After adding the texts, these texts can also be fetched as results if their
-# embeddings are similar to the embedding of the query. Currently the frontend is
-# designed so as to accommodate these as well. 
+# embeddings are similar to the embedding of the query. Currently, the frontend is
+# designed to accommodate these as well.
 
 # Adding texts
 # texts = [
-#     'A dense forest',
-#     'A beautiful beach',
-#     'people playing games',
-#     'Students syudying in class',
-#     'a beautiful painting',
-#     'place with scenic beauty',
-#     'confident woman',
-#     'cute little creature',
-#     'players playing badminton'
+#    'A dense forest',
+#    'A beautiful beach',
+#    'people playing games',
+#    'Students syudying in class',
+#    'a beautiful painting',
+#    'place with scenic beauty',
+#    'confident woman',
+#    'cute little creature',
+#    'players playing badminton'
 # ]
-# for txt in texts:
-#     data_properties = {
-#         "text":txt
-#     }
-#     client.data_object.create(data_properties, "ClipExample", generate_uuid('ClipExample',txt))
+# texts_to_add = []
+# for t in texts:
+#    text = wvc.DataObject(
+#        properties={
+#            "text": t
+#        })
+#    texts_to_add.append(text)
+
+# images.data.insert_many(texts_to_add)
 # print("Texts added")
--- upload.py
+++ upload.py
@@ -1,83 +1,88 @@
 import os
 from app import app
-import urllib.request
-from flask import Flask, flash, request, redirect, url_for, render_template
+from flask import flash, request, redirect, url_for, render_template
 from werkzeug.utils import secure_filename
-import weaviate
-from test import testImage, testText
-ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif','jfif'])
+from util import search_by_text,search_by_image
+
+ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg', 'gif', 'jfif'])
 
 
 def allowed_file(filename):
-	return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
-	
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
 @app.route('/')
 def upload_form():
-	return render_template('upload.html')
+    return render_template('upload.html')
 
-@ app.route('/text_description', methods=['POST'])
+
+@app.route('/text_description', methods=['POST'])
 def text_description():
-	# This function uses the testText function to get results for a text query.
-	# This function has been designed taking into consideration that some users might
-	# also add text data to weaviate and then the results would contain text as well as images.
+    # This function uses the testText function to get results for a text query.
+    # This function has been designed taking into consideration that some users might
+    # also add text data to weaviate and then the results would contain text as well as images.
 
-	text = request.form.get("description")
-	
-	text_results = testText({"concepts":[text]})
-	# Using two lists to store image result and text result
-	images = []
-	texts = []
-	sym = ['.jfif','.jpg','.jpeg','.png']
-	for i in text_results:
-		add = 0
-		for s in sym:
-			if s in i:
-				images.append(i)
-				add = 1
-				break
-		if add==0:
-			texts.append(i)
-	# Passing text result and image names to upload.html page
-	return render_template('upload.html', description=text,images=images,texts=texts)
+    text = request.form.get("description")
 
-@app.route('/', methods=['POST','GET'])
+    text_results = search_by_text(text)
+    # Using two lists to store image result and text result
+    images = []
+    texts = []
+    sym = ['.jfif', '.jpg', '.jpeg', '.png']
+    for i in text_results:
+        add = 0
+        for s in sym:
+            if s in i:
+                images.append(i)
+                add = 1
+                break
+        if add == 0:
+            texts.append(i)
+    # Passing text result and image names to upload.html page
+    return render_template('upload.html', description=text, images=images, texts=texts)
+
+
+@app.route('/', methods=['POST', 'GET'])
 def upload_image():
     # Function to upload an image. You can upload images from test folder or from the internet and use them.
-	# This function also uses the testImage function to get the top 3 similar image names from weaviate.
-	# These are then passed to the upload.html page so as to display them to the user.
-	if 'file' not in request.files:
-		flash('No file part')
-		return redirect(request.url)
+    # This function also uses the testImage function to get the top 3 similar image names from weaviate.
+    # These are then passed to the upload.html page so as to display them to the user.
+    if 'file' not in request.files:
+        flash('No file part')
+        return redirect(request.url)
 
-	file = request.files['file']
+    file = request.files['file']
 
-	if file.filename == '':
-		flash('No image selected for uploading')
-		return redirect(request.url)
+    if file.filename == '':
+        flash('No image selected for uploading')
+        return redirect(request.url)
 
-	if file and allowed_file(file.filename):
-		filename = secure_filename(file.filename)
-		file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-		
-		flash('Image successfully uploaded and displayed below')
-		print(" ==========\n",'File saved\n',"==========\n")
-		
-		# Using the testImage in the line below.
-		return render_template('upload.html', filename=filename,imagePath=testImage({"image":"static/uploads/{}".format(filename)}))
+    if file and allowed_file(file.filename):
+        filename = secure_filename(file.filename)
+        file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
 
-	else:
-		flash('Allowed image types are -> png, jpg, jpeg, gif, jfif')
-		return redirect(request.url)
+        flash('Image successfully uploaded and displayed below')
+        print(" ==========\n", 'File saved\n', "==========\n")
+
+        # Using the testImage in the line below.
+        return render_template('upload.html', filename=filename,
+                               imagePath=search_by_image(f"static/uploads/{filename}"))
+
+    else:
+        flash('Allowed image types are -> png, jpg, jpeg, gif, jfif')
+        return redirect(request.url)
+
 
 @app.route('/display/<filename>')
-def display_image(filename,uploaded=True):
+def display_image(filename, uploaded=True):
     # Function to display uploaded image
     print("Display image called")
 
     if uploaded:
-	    print("Uploaded")
+        print("Uploaded")
 
     return redirect(url_for('static', filename='uploads/' + filename), code=301)
 
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
